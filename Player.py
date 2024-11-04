1dbe1ff3c79f8c5fe383252fe6776020472b86fa
cwwong <cwwong@lscm.hk>
2024-11-04 16:56:43 +0800

    create Player.py

---
+++ Player.py
@@ -0,0 +1,10 @@
+import pygame
+
+# player class inherits from pygame.sprite
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        #TODO: initiate sprite
+
+    def move():
+        pass
